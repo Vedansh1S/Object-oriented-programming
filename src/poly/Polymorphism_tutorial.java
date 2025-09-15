e2147293a1e836cf9b3c5637be5b62fec3486934
Vedansh Sharma <sharmavedansh113@gmail.com>
2025-09-15 22:01:49 +0530

    add interface to codebase.

--- src/poly/Polymorphism_tutorial.java
+++ src/poly/Polymorphism_tutorial.java
@@ -1,29 +1,29 @@
 package poly;
 
 class Animal {
-    public static void makeSound() {
+    public void makeSound() { // When declared static, this method cannot be overridden
         System.out.println("The animal makes a generic sound.");
     }
 }
 
 class Dog extends Animal {
-    //@Override
-    public static void makeSound() {
+    @Override
+    public void makeSound() {
         System.out.println("The dog barks: Woof! Woof!");
     }
 }
 
 class Cat extends Animal {
-   // @Override
-    public static void makeSound() {
+    @Override
+    public void makeSound() {
         System.out.println("The cat meows: Meow!");
     }
 }
 
 public class Polymorphism_tutorial {
     public static void main(String[] args) {
-        Animal myAnimal;               // 6. A reference of the superclass type
-        myAnimal = new Animal();     // 6. Pointing to an Animal object
+        Animal myAnimal;              // 6. A reference of the superclass type
+        myAnimal = new Animal();      // 6. Pointing to an Animal object
         myAnimal.makeSound();         // This will call the Animal's version of makeSound() Output: The animal makes a generic sound.
 
         myAnimal = new Dog();         // 7. Pointing to a Dog object
